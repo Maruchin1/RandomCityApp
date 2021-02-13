206734cdca3a677df38697eb8e9fd3345ac998cc
Maruchin1 <marcinpk.mp@gmail.com>
2021-02-13 17:44:05 +0100

    Animations in cities list

--- app/src/main/java/com/example/randomcityapp/presentation/RandomCitiesListAdapter.kt
+++ app/src/main/java/com/example/randomcityapp/presentation/RandomCitiesListAdapter.kt
@@ -2,19 +2,21 @@ package com.example.randomcityapp.presentation
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.library.baseAdapters.BR
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.randomcityapp.core.RandomCity
 import com.example.randomcityapp.databinding.ItemRandomCityBinding
-import com.example.randomcityapp.presentation.framework.BindingViewHolder
 
 class RandomCitiesListAdapter(
     private val fragment: Fragment,
     private val source: LiveData<List<RandomCity>>
-) : RecyclerView.Adapter<BindingViewHolder>() {
+) : RecyclerView.Adapter<RandomCitiesListAdapter.BindingViewHolder>() {
 
     private val itemsList = mutableListOf<RandomCity>()
+    private val diffCallback = CitiesDiffCallback()
 
     init {
         source.observe(fragment.viewLifecycleOwner) { updateItems(it) }
@@ -36,8 +38,53 @@ class RandomCitiesListAdapter(
     }
 
     private fun updateItems(newList: List<RandomCity>) {
+        diffCallback.setLists(itemsList, newList)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
         itemsList.clear()
         itemsList.addAll(newList)
-        notifyDataSetChanged()
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    class BindingViewHolder(
+        private val binding: ItemRandomCityBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: RandomCity, controller: Fragment) = binding.run {
+            setVariable(BR.item, item)
+            setVariable(BR.controller, controller)
+            executePendingBindings()
+        }
+    }
+
+    private class CitiesDiffCallback : DiffUtil.Callback() {
+
+        private var oldList: List<RandomCity> = emptyList()
+        private var newList: List<RandomCity> = emptyList()
+
+        fun setLists(oldList: List<RandomCity>, newList: List<RandomCity>) {
+            this.oldList = oldList
+            this.newList = newList
+        }
+
+        override fun getOldListSize(): Int {
+            return oldList.size
+        }
+
+        override fun getNewListSize(): Int {
+            return newList.size
+        }
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldList[oldItemPosition]
+            val newItem = newList[newItemPosition]
+            return oldItem.emissionDateTime == newItem.emissionDateTime
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldList[oldItemPosition]
+            val newItem = newList[newItemPosition]
+            return oldItem == newItem
+        }
+
     }
 }
