92d5821534f44eae9b783289c7a4189b4d9d0fd0
Maruchin1 <marcinpk.mp@gmail.com>
2021-02-13 19:47:51 +0100

    Setup Google Maps

--- app/src/main/java/com/example/randomcityapp/presentation/DetailsFragment.kt
+++ app/src/main/java/com/example/randomcityapp/presentation/DetailsFragment.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.randomcityapp.R
 import com.example.randomcityapp.core.MainViewModel
 import com.example.randomcityapp.databinding.FragmentDetailsBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
-class DetailsFragment : Fragment() {
+
+class DetailsFragment : Fragment(), OnMapReadyCallback {
 
     private val viewModel: MainViewModel by sharedViewModel()
     private var binding: FragmentDetailsBinding? = null
@@ -27,8 +34,23 @@ class DetailsFragment : Fragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
     }
 }
