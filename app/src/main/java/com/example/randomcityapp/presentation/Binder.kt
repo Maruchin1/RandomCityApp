63a2e8db8f764356af49951665fb4be59e3c8fa8
Maruchin1 <marcinpk.mp@gmail.com>
2021-02-13 18:59:24 +0100

    Dynamic toolbar color in details

--- app/src/main/java/com/example/randomcityapp/presentation/Binder.kt
+++ app/src/main/java/com/example/randomcityapp/presentation/Binder.kt
@@ -1,7 +1,10 @@
 package com.example.randomcityapp.presentation
 
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.example.randomcityapp.R
+import com.google.android.material.appbar.MaterialToolbar
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -15,5 +18,25 @@ object Binder {
             val text = dateTime.format(dateTimeFormatter)
             textView.text = text
         }
+    }
+
+    @BindingAdapter("app:toolbarColor")
+    @JvmStatic
+    fun setToolbarColor(toolbar: MaterialToolbar, color: String?) {
+        val colorId = when (color) {
+            "Yellow" -> R.color.yellow
+            "Green" -> R.color.green
+            "Blue" -> R.color.blue
+            "Red" -> R.color.red
+            "Black" -> R.color.black
+            "White" -> R.color.white
+            else -> throw IllegalStateException("Invalid color: $color")
+        }
+        val textColorId = when (color) {
+            "Yellow", "White" -> R.color.black
+            else -> R.color.white
+        }
+        toolbar.setBackgroundColor(ContextCompat.getColor(toolbar.context, colorId))
+        toolbar.setTitleTextColor(ContextCompat.getColor(toolbar.context, textColorId))
     }
 }
